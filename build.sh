6b6906a45005279a44e7b657adf0c7a7c5b60ee1
aisinker <aisinker@outlook.com>
2026-02-16 14:57:32 +0800

    update

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 npm run docs:build
 cp src/turnstile.html docs/.vitepress/dist/turnstile.html
 cp src/password-reset.html docs/.vitepress/dist/password-reset.html
-cp src/version.json docs/.vitepress/dist/version.json
+cp src/latest-version.json docs/.vitepress/dist/latest-version.json
