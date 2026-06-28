34546b7aa6cae070806f931557988a73a78b58e8
aisinker <aisinker@outlook.com>
2026-06-28 23:58:34 +0800

    chore: bump to v1.0.0-beta.4

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -16,12 +16,12 @@ export default {
           "home-features-before": () => h(ScreenshotGallery),
           "home-features-after": () => [
             h(DownloadPanel, {
-              downloadUrl: "https://github.com/virmath/docs/releases/download/v1.0.0-beta.3/Virmath-Elf-v1.0.0-beta.3-win-x64-setup.exe",
-              chinaDownloadUrl: "https://gh-proxy.org/https://github.com/virmath/docs/releases/download/v1.0.0-beta.3/Virmath-Elf-v1.0.0-beta.3-win-x64-setup.exe",
+              downloadUrl: "https://github.com/virmath/docs/releases/download/v1.0.0-beta.4/Virmath-Elf-v1.0.0-beta.4-win-x64-setup.exe",
+              chinaDownloadUrl: "https://gh-proxy.org/https://github.com/virmath/docs/releases/download/v1.0.0-beta.4/Virmath-Elf-v1.0.0-beta.4-win-x64-setup.exe",
               allDownloadsUrl: "docs/downloads",
               changelogUrl: "docs/changelog",
-              version: "v1.0.0-beta.3",
-              releaseDate: "2026-06-26",
+              version: "v1.0.0-beta.4",
+              releaseDate: "2026-06-28",
               platforms: "Windows 10 22H2 x64",
             }),
             h(PricingPanel, {
